5aa8b16dfaa8e7b32cc8c9450216f4992f61ae09
时间小鱼 <dubboblue@gmail.com>
2020-12-15 17:33:51 +0800

    初始化项目

---
+++ index.tsx
@@ -0,0 +1,4 @@
+import * as ReactDOM from 'react-dom';
+import App from './src/index';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { resolve } = path;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.tsx',
+    output: {
+        filename: '[name].js',
+        path: resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts[x]?/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, './public/index.html'),
+        }),
+    ],
+};
